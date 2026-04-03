b483e3b4762882fc0756edab448bffbda432950c
diendue48k <levinhdien48kdue@gmail.com>
2026-04-03 15:13:02 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { FormConfigProvider } from './contexts/FormConfigContext';
 import Layout from './components/Layout';
@@ -28,7 +28,7 @@ export default function App() {
   return (
     <AuthProvider>
       <FormConfigProvider>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
@@ -40,7 +40,7 @@ export default function App() {
             </Route>
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </FormConfigProvider>
     </AuthProvider>
   );
